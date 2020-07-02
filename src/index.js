e1ef10028d8cc4106062da81de0029212bff2854
ricomen <ricomen@mail.ru>
2020-07-02 20:51:49 +0500

    Разметка

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 
-const el = <h1>Hello World</h1>
+const el = (
+    <>
+        <h1>Список дел</h1>
+        <input type="text" placeholder='Поиск'/>
+        <ul>
+            <li>Изучение React</li>
+            <li>Создание приложения</li>
+        </ul>
+    </>
+);
 ReactDOM.render(el, document.getElementById('root'));
