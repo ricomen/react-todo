b6c4b0464a6e7849a26eaf155dd77fbbff2a0eff
ricomen <ricomen@mail.ru>
2020-07-04 20:03:14 +0500

    Структура проекта

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
+import ItemStatusFilter from '../item-status-filter';
 import ToDoList from "../todo-list";
 
 const App = () => {
@@ -23,11 +24,14 @@ const App = () => {
         },
     ];
     return (
-        <>
+        <div className='todo-app'>
             <AppHeader />
-            <SearchPanel />
+            <div className="search-panel d-flex">
+                <SearchPanel />
+                <ItemStatusFilter />
+            </div>
             <ToDoList todos={todoData} />s
-        </>
+        </div>
     )
 };
 
