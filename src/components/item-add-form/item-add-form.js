28b9593591fd500e743a5127bf803e79fc1a1b87
ricomen <ricomen@mail.ru>
2020-07-07 22:07:46 +0500

    Контролируемый компонент

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,15 +3,42 @@ import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
 
-    render() {
-        const { onItemAdded } = this.props;
+    state = {
+        label: ''
+    }
 
+    onLabelChange = (evt) => {
+        this.setState({
+            label: evt.target.value
+        });
+    };
+
+    onSubmit = (evt) => {
+        evt.preventDefault();
+        this.props.onItemAdded(this.state.label);
+        this.setState(() => {
+            return {
+                label: ''
+            };
+        });
+    };
+
+    render() {
         return (
-            <div className='item-add-form'>
+            <form
+                className='item-add-form d-flex'
+                onSubmit={this.onSubmit}>
+                <input
+                    className='form-control'
+                    type='text'
+                    placeholder='Что нужно сделать?'
+                    onChange={this.onLabelChange}
+                    value={this.state.label}
+                />
                 <button
-                    className='btn btn-outline-seconfary btn-sm'
-                    onClick={() => onItemAdded('Hello World')}>Добавить задачу</button>
-            </div>
+                    className='btn btn-outline-secondary btn-sm'
+                >Добавить задачу</button>
+            </form>
         );
     };
 }
