cafb159e6aa868654abcf88efeded4a31658bf90
ricomen <ricomen@mail.ru>
2020-07-07 19:10:29 +0500

    Удаление элемента из массива

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,40 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import ItemStatusFilter from '../item-status-filter';
 import ToDoList from "../todo-list";
 
-const App = () => {
-    const todoData = [
-        {
-            label: 'DrinkCofeee',
-            important: false,
-            id: 1
-        },
-        {
-            label: 'Make Awesome App',
-            important: true,
-            id: 2
-        },
-        {
-            label: 'Drink Teal',
-            impotant: false,
-            id: 3
-        },
-    ];
-    return (
-        <div className='todo-app'>
-            <AppHeader />
-            <div className="search-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
-            </div>
-            <ToDoList
-                todos={todoData}
-                onDeleted={(id) => {console.log(id)}}/>
-        </div>
-    )
-};
+export default class App extends Component {
 
-export default App;
+    state = {
+        todoData: [
+            {
+                label: 'DrinkCofeee',
+                important: false,
+                id: 1
+            },
+            {
+                label: 'Make Awesome App',
+                important: true,
+                id: 2
+            },
+            {
+                label: 'Drink Teal',
+                impotant: false,
+                id: 3
+            },
+        ]
+    };
+
+    deleteItem = (id) => {
+        this.setState(({todoData}) => {
+            const idx = todoData.findIndex((el) => el.id ===id);
+            const newArray = [ ...todoData.slice(0, idx), ...todoData.slice(idx+1)]
+
+            return {
+                todoData: newArray
+            }
+        });
+    };
+
+    render() {
+        return (
+            <div className='todo-app'>
+                <AppHeader />
+                <div className="search-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+                <ToDoList
+                    todos={this.state.todoData}
+                    onDeleted={this.deleteItem}/>
+            </div>
+        )
+    }
+};
